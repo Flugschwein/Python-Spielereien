b16e8cf04b13451b38cb052767b9a72f530d24be
Anton_PC <Anton@PC.com>
2017-06-21 14:18:32 +0200

    test phase

--- every gif color.py
+++ every gif color.py
@@ -1,10 +1,15 @@
 html = open('hey.html', 'a')
 html.write('<TABLE BORDER=0 CELLPADDING=10 CELLSPACING=0>\n    <TR>\n')
-atm = 0x000000
+atm = 0
 for i in range(16):
     for j in range(16):
-        string = str(atm)
+        string = str(hex(atm)[2:])
+        if len(string) != 8:
+            for i in range(8-len(string)):
+                string = "0"+string
+        atm += 0xffffff//256
         html.write('      <TD WIDTH="10" HEIGHT="10" BGCOLOR=#')
         html.write(string)
-        html.write('>')
+        html.write('>\n      &nbsp;\n      </TD>\n')
+    html.write('    </TR>\n    <TR>\n')
 html.close()
