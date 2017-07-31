d41751d1d550780c62946c0267bdcd747ff2f05f
Anton_PC <Anton@PC.com>
2017-07-31 17:16:30 +0200

    Droptable change tester

--- Warframe Droptable change detector.py
+++
@@ -1,9 +0,0 @@
-import bs4
-from urllib.request import urlopen
-
-
-def download_tables():
-    droptables = urlopen('https://n8k6e2y6.ssl.hwcdn.net/repos/hnfvc0o3jnfvc873njb03enrf56.html')
-    droptables = bs4.BeautifulSoup(droptables, 'html.parser')
-    return droptables
-print(download_tables().prettify())
---
+++ Warframe/Warframe Droptable change detector.py
@@ -0,0 +1,42 @@
+from urllib.request import urlopen
+import time
+import bs4
+import shelve
+
+
+def download_tables():
+    print('Downloading droptables...')
+    droptables = urlopen('https://n8k6e2y6.ssl.hwcdn.net/repos/hnfvc0o3jnfvc873njb03enrf56.html')
+    print('Processing droptables...')
+    droptables = bs4.BeautifulSoup(droptables, 'html.parser')
+    return droptables.prettify()
+
+
+def loop():
+    cur = ''
+    prev = download_tables()
+    i = 0
+    start = time.time()
+    print(start)
+    while True:
+        cur = download_tables()
+        if cur == prev:
+            print('Nothing changed.\nWaiting 5 minutes. Then trying again.')
+            time.sleep(300)
+            prev = cur
+            continue
+        else:
+            print('yeah')
+            i += 1
+            with open('old-droptables.html', 'w') as old, open('new-droptables.html', 'w') as new, shelve.open('times') as shelf:
+                shelf['start'] = start
+                shelf['change' + str(i)] = time.time()
+                old.write(prev)
+                new.write(cur)
+                old.close()
+                new.close()
+                shelf.close()
+            prev = cur
+            continue
+loop()
+    
