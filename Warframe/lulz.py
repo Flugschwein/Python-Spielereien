56c591ee7986c8f70a010f0ea1b7a0a72bca76d1
Anton_PC <Anton@PC.com>
2017-07-31 23:50:41 +0200

    bg

--- Warframe/lulz.py
+++
@@ -1,6 +0,0 @@
-from urllib.request import urlopen
-import bs4
-import pprint
-droptables = urlopen('https://n8k6e2y6.ssl.hwcdn.net/repos/hnfvc0o3jnfvc873njb03enrf56.html')
-droptables = bs4.BeautifulSoup(droptables, 'html.parser')
-pprint.pprint(droptables.find_all('a'))
