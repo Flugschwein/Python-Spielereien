40b7209b3c347c8667e1421e3fa2f1a0bd58f3f1
Your Name <you@example.com>
2017-07-31 16:44:50 +0000

    modified:   Warframe Droptable change detector.py

--- Warframe/Warframe Droptable change detector.py
+++ Warframe/Warframe Droptable change detector.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 from urllib.request import urlopen
 import time
 import bs4
@@ -6,7 +7,11 @@ import shelve
 
 def download_tables():
     print('Downloading droptables...')
-    droptables = urlopen('https://n8k6e2y6.ssl.hwcdn.net/repos/hnfvc0o3jnfvc873njb03enrf56.html')
+    try:
+        droptables = urlopen('https://n8k6e2y6.ssl.hwcdn.net/repos/hnfvc0o3jnfvc873njb03enrf56.html')
+    except:
+        print('Downloading failed!\nTrying again in 5 minutes')
+        
     print('Processing droptables...')
     droptables = bs4.BeautifulSoup(droptables, 'html.parser')
     return droptables.prettify()
@@ -20,7 +25,10 @@ def loop():
     print(start)
     while True:
         cur = download_tables()
-        if cur == prev:
+        if cur == None:
+            time.sleep(300)
+            continue
+        elif cur == prev:
             print('Nothing changed.\nWaiting 5 minutes. Then trying again.')
             time.sleep(300)
             prev = cur
